f7d4a014689c1794bfbf90635db32efbd73fe0f8
tongyuxin1 <tongyuxin1@gmail.com>
2018-12-04 09:32:11 +0800

    Create Main.java

---
+++ src/main/java/com/example/demo/decorator2/Main.java
@@ -0,0 +1,18 @@
+package com.example.demo.decorator2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=========我是土豪都给我加上===========");
+        IPancake pancake = new Pancake();
+        IPancake pancakeWithEgg = new EggDecorator(pancake);
+        IPancake pancakeWithEggAndHam = new HamDecorator(pancakeWithEgg);
+        IPancake panckeWithEggAndHamAndLettuce = new LettuceDecorator(pancakeWithEggAndHam);
+        panckeWithEggAndHamAndLettuce.cook();
+
+        System.out.println("==========我是程序猿，加两个鸡蛋补补==============");
+        IPancake pancake2 = new Pancake();
+        IPancake pancakeWithEgg2 = new EggDecorator(pancake2);
+        IPancake pancakeWithTwoEgg = new EggDecorator(pancakeWithEgg2);
+        pancakeWithTwoEgg.cook();
+    }
+}
